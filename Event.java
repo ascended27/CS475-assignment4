d069d65e2544ddc73f1b5dfc9529a4da9f3086f1
ascended27 <ascended27@gmail.com>
2017-11-26 20:27:15 -0500

    Initial Classes

---
+++ Event.java
@@ -0,0 +1,12 @@
+/**
+ * The Event class will contain information
+ * about an event such as: Title, Name, Time,
+ * and Attendees.
+ *
+ * This class will need to be synchronus. The owner
+ * may need to write to their calendar while another
+ * user attempting to reading.
+ */
+public class Event {
+
+}
---
+++ Server.java
@@ -0,0 +1,8 @@
+/**
+ * This class will act as the server that interacts
+ * with all the clients. It will handle the creation
+ * of new calendars and the creation of events.
+ */
+public class Server{
+
+}
