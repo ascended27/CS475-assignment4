9e39509bb2904af056bb2f0540b3f45a03a2925b
ascended27 <ascended27@gmail.com>
2017-11-29 13:47:46 -0500

    Added user functions

--- User.java
+++ User.java
@@ -2,4 +2,16 @@ public class User {
 
     public String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+
+        this.name = name;
+    }
 }
