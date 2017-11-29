5ecb5736c8e67afe5810e325a48c5c9cf94edcd3
ascended27 <ascended27@gmail.com>
2017-11-29 14:21:34 -0500

    Added Server

--- Server.java
+++ Server.java
@@ -1,8 +1,23 @@
+import java.rmi.Naming;
+
 /**
  * This class will act as the server that interacts
  * with all the clients. It will handle the creation
  * of new calendars and the creation of events.
  */
 public class Server{
+
+    //TODO: Test this?
+    public static void main(String argv[]){
+        try{
+            System.setSecurityManager(new SecurityManager());
+            System.out.println("Server: Registering Calendar Service");
+            CalendarManager cm = new CalendarManagerImpl();
+            Naming.rebind("CalendarService",cm);
+            System.out.println("Server: Ready...");
+        } catch (Exception e){
+            System.out.println("Server-Error: " + e);
+        }
+    }
 
 }
